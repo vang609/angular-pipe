b5e7ab21dc4119d6f27f0a58a381f4ed4037bce4
Enrique Vargas <enrique.vangz@gmail.com>
2020-04-30 03:00:59 -0400

    changes 01
    changes 01

--- src/app/root-store/actions/message.actions.ts
+++
@@ -1,7 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-import { Message } from '../../shared/models/message.model';
-
-export const setMessage = createAction(
-    '[Message] Set Message',
-    props< { message: Message } >()
-);
--- src/app/shared/constant/url.constant.ts
+++
@@ -1,5 +0,0 @@
-export const hostURL = 'https://localhost:44311';
-
-export const URL = {
-    MCCI_SRC : '/api/MCCInquiry/V1/merchantcategorycode?' // Param: mccDescription, transProcType
-};
