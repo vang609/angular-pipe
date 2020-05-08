c5ddb76c88949f7902eb476d37bba7b6e619ff28
Enrique Vargas <enrique.vangz@gmail.com>
2020-05-07 23:52:04 -0400

    example
    example

---
+++ src/app/root-store/reducers/index.ts
@@ -0,0 +1,2 @@
+export * from './loading.reducer';
+export * from './message.reducer';
---
+++ src/app/shared/constant/url.constant.ts
@@ -0,0 +1,5 @@
+export const hostURL = 'https://localhost:44311';
+
+export const URL = {
+    MCCI_SRC: `${hostURL}/api/MCCInquiry/V1/merchantcategorycode?` // Param: mccDescription, transProcType
+};
