a24bd076274270aac198bf34df266e3352cff43c
Enrique Vargas <enrique.vangz@gmail.com>
2020-05-09 15:07:42 -0400

    test01
    test01

--- src/app/shared/constant/url.constant.ts
+++
@@ -1,5 +0,0 @@
-export const hostURL = 'https://localhost:44311';
-
-export const URL = {
-    MCCI_SRC: `${hostURL}/api/MCCInquiry/V1/merchantcategorycode?` // Param: mccDescription, transProcType
-};
