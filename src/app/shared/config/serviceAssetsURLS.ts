33747615162d11b8be42e7071212ee8188d56ba6
Enrique Vargas <enrique.vangz@gmail.com>
2020-05-11 23:49:53 -0400

    review
    review

--- src/app/shared/config/serviceAssetsURLS.ts
+++
@@ -1,7 +0,0 @@
-import { ENDPOINT_ASSETS } from './envAPIContextExternal';
-
-
-export const SERVICE_ASSETS_PATH = {
-    forBasicMerchantProfile:            ENDPOINT_ASSETS + '/json/basic-merchant.json',
-    forMccFinder:                       ENDPOINT_ASSETS + '/json/mcc-data.json'
-};
--- src/app/shared/config/serviceURLS.ts
+++
@@ -1,6 +0,0 @@
-import { ENDPOINT_URI } from './envAPIContext';
-
-export const SERVICE_URLS = {
-    forBasicMerchantProfile:            ENDPOINT_URI + '/api/MCCInquiry/V1/merchantcategorycode?',
-    forMccFinder:                       ENDPOINT_URI + '/api/MCCInquiry/V1/merchantcategorycode?',
-};
